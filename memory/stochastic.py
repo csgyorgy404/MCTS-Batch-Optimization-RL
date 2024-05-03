369916b46759ef32fa8a6e33e12513cf84d3daf5
Gyuri <csgyorgy404@gmail.com>
2024-05-03 19:32:20 +0200

    Refactor Buffer class to use GPU if available

--- memory/stochastic.py
+++ memory/stochastic.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 import pandas as pd
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class Buffer:
     def __init__(self, env, memory_size, batch_size):
@@ -16,22 +17,37 @@ class Buffer:
 
         self.memory = pd.concat([self.memory, pd.DataFrame([new_row])], ignore_index=True)
 
+    # def sample(self):
+    #     batch = self.memory.sample(self.batch_size)
+
+    #     # state = torch.tensor(np.stack(batch['state'].values), dtype=torch.float32)
+    #     # action = torch.tensor(np.stack(batch['action'].values), dtype=torch.int64)
+    #     # next_state = torch.tensor(np.stack(batch['next_state'].values), dtype=torch.float32)
+    #     # reward = torch.tensor(np.stack(batch['reward'].values), dtype=torch.float32)
+    #     # done = torch.tensor(np.stack(batch['done'].values), dtype=torch.int64)
+
+    #     state = torch.tensor(batch['state'].tolist()).to(device)
+    #     action = torch.tensor(batch['action'].tolist()).to(device)
+    #     next_state = torch.tensor(batch['next_state'].tolist()).to(device)
+    #     reward = torch.tensor(batch['reward'].tolist()).to(device)
+    #     done =  torch.LongTensor(batch['done'].tolist()).to(device)
+
+    #     return state, action, reward, next_state, done
     def sample(self):
         batch = self.memory.sample(self.batch_size)
 
-        # state = torch.tensor(np.stack(batch['state'].values), dtype=torch.float32)
-        # action = torch.tensor(np.stack(batch['action'].values), dtype=torch.int64)
-        # next_state = torch.tensor(np.stack(batch['next_state'].values), dtype=torch.float32)
-        # reward = torch.tensor(np.stack(batch['reward'].values), dtype=torch.float32)
-        # done = torch.tensor(np.stack(batch['done'].values), dtype=torch.int64)
+        # Convert batch DataFrame to numpy array
+        batch_np = batch.to_numpy()
 
-        state = torch.tensor(batch['state'].tolist())
-        action = torch.tensor(batch['action'].tolist())
-        next_state = torch.tensor(batch['next_state'].tolist())
-        reward = torch.tensor(batch['reward'].tolist())
-        done =  torch.LongTensor(batch['done'].tolist())
+        # Extract columns from the numpy array
+        state = torch.tensor(batch_np[:, 0].tolist()).to(device)
+        action = torch.tensor(batch_np[:, 1].tolist()).to(device)
+        next_state = torch.tensor(batch_np[:, 2].tolist()).to(device)
+        reward = torch.tensor(batch_np[:, 3].tolist()).to(device)
+        done = torch.LongTensor(batch_np[:, 4].tolist()).to(device)
 
         return state, action, reward, next_state, done
+
 
     def fill(self, agent):
         num_of_samples = 0
